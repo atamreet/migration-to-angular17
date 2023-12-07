b3f83f4d34b81e4781974bdf16676c962722218d
Amreet <amreet.singh54@outlook.com>
2023-12-07 13:12:50 -0500

    added vite

---
+++ angular-project/shell-project/vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite';
+import angularPlugin from 'some-angular-vite-plugin'; // Replace with the actual Angular plugin
+
+export default defineConfig({
+  plugins: [angularPlugin()],
+  // Additional configurations as needed
+});
